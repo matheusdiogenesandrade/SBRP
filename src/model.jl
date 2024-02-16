0229252bd9d6dd8e4d7387173ab361d200c3f177
Matheus Diógenes Andrade <matheusdiogenesandrade@pop-os.localdomain>
2024-02-16 20:55:29 +0100

    reducing memoery consumption

--- src/model.jl
+++ src/model.jl
@@ -5,6 +5,16 @@ using DataStructures
 
 const EPS = 1e-4
 
+function unsetBinary(vars)
+    unset_binary.(vars)
+    set_lower_bound.(vars, zeros(length(vars)))
+    set_upper_bound.(vars, ones(length(vars)))
+end
+
+function setBinary(vars)
+    set_binary.(vars)
+end
+
 #=
 Get all blocks with some node inside the component ´S´
 input: 
